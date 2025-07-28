2a129374bcd7dc6440d78357e1e64667082e9b8b
Greg Gilbert <gjgilbert@astro.ucla.edu>
2025-07-28 12:02:58 -0700

    developing detrend module

--- src/modules/detrend/detrend.py
+++ src/modules/detrend/detrend.py
@@ -1,12 +1,21 @@
-__all__ = ['Detrend']
+__all__ = ['SimpleDetrender',
+           'GaussianProcessDetrender',
+           'AutoCorrelationDetrender'
+           ]
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import aesara_theano_fallback.tensor as T
 from astropy.stats import sigma_clip, mad_std
 from astropy.timeseries import LombScargle
+from celerite2.backprop import LinAlgError
+from celerite2.theano import GaussianProcess
+from celerite2.theano import terms as GPterms
 import numpy as np
+import pymc3 as pm
+import pymc3_ext as pmx
 from scipy.signal import medfilt as median_filter
 from scipy.signal import savgol_filter
 from src.schema.litecurve import LiteCurve
@@ -35,15 +44,18 @@ class SimpleDetrender:
             self.durs.append(p.duration)
 
     
-    def make_transit_mask(self, rel_size=None, abs_size=None):
+    def make_transit_mask(self, rel_size=None, abs_size=None, mask_type='standard'):
         """
         Arguments
             rel_size : full width of masked region in units of transit duration
             abs_size : full width of masked region in units of time
-            ignore_bad (bool) : True to exclude bad quality transits from mask
+            mask_type : type of transit mask to return (default = 'standard')
         
-        mask shape is (num_planets, num_cadences)
-        boolean (1 = near transit, 0 = far from transit)
+        Several output mask_type are supported
+         * standard : bool, shape (n_planet, n_cadence), True near transits
+         * condensed : bool, shape (n_cadence), True near transit
+         * count : int, shape (n_cadence), value is number of planets near transit
+         * overlap : bool, shanpe (n_cadnece), True if multiple planets are near transit
         """
         if (rel_size is None) & (abs_size is None):
             raise ValueError("either rel_size or abs_size must be provided")
@@ -59,6 +71,17 @@ class SimpleDetrender:
             half_width = 0.5*np.max([rel_size*p.duration, abs_size])
             for tc in p.ephemeris.ttime:
                 mask[n] += np.abs(self.litecurve.time - tc) < half_width
+
+        if mask_type == 'standard':
+            mask = mask.astype(bool)
+        elif mask_type == 'condensed':
+            mask = np.sum(mask, axis=0).astype(bool)
+        elif mask_type == 'count':
+            mask = np.sum(mask, axis=0)
+        elif mask_type == 'overlap':
+            mask = np.sum(mask, axis=0) > 0
+        else:
+            raise ValueError(f"mask_type f{mask_type} not supported")
 
         return mask.astype(bool)
 
@@ -142,15 +165,64 @@ class SimpleDetrender:
             if count >= 3:
                 loop = False
 
-        return lc
-    
+        self.litecurve = lc
 
-    def estimate_oscillation_period(self, min_period):
+
+    def _identify_breaks(self, gap_tolerance, jump_tolerance, mask=None):
+        """
+        Identify gaps (breaks in times) and jumps (sudden flux changes) in a litecurve
+            * GAPS are consecutive missing cadences in the TIME domain
+            * JUMPS are large cadence-to-cadence FLUX variations
+
+        Arguments
+            gap_tolerance : int
+                number of cadences to be considered a (time) gap
+            jump_tolerance : float
+                sigma threshold for identifying (flux) jumps
+            mask : array-like, bool (optional)
+                1 near transit, 0 far from transit
+        """
+        # identify GAPS in time
+        gaps = np.pad(np.diff(self.litecurve.cadno), (1,0), 'constant', constant_values=(1,0))
+        gap_locs = np.pad(
+            np.where(gaps > gap_tolerance)[0], (1,1), 'constant', constant_values=(0, len(gaps)+1)
+        )
+
+        # identify JUMPS in flux
+        jumps = np.pad(np.diff(self.litecurve.flux), (1,0), 'constant', constant_values=(0,0))
+        jump_size = np.abs(jumps - np.median(jumps)) / mad_std(jumps)
+        jump_locs = np.where(~mask.astype(bool) & (jump_size > jump_tolerance))[0]
+
+        # combine breaks
+        break_locs = np.sort(np.unique(np.hstack([gap_locs, jump_locs])))
+        
+        # fix edge behavior
+        bad = np.hstack([False, np.diff(break_locs) < gap_tolerance])
+        if bad[-1]:
+            bad[-2:] = (True, False)
+        break_locs = break_locs[~bad]
+        break_locs[-1] -= 2
+
+        assert break_locs[0] == 0, "unexpected edge behavior"
+        assert break_locs[-1] == len(self.litecurve.cadno) - 1, "unexpected edge behavior"
+        
+        return break_locs
+
+
+class GaussianProcessDetrender(SimpleDetrender):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    def estimate_oscillation_period(self, min_period=None):
         """
         Docstring
         """
         lc = self.litecurve
         lombscargle = LombScargle(lc.time, lc.flux)
+
+        if min_period is None:
+            min_period = 3 * np.max(self.durs)
 
         min_freq = 1 / (lc.time.max() - lc.time.min())
         max_freq = 1 / min_period
@@ -165,11 +237,135 @@ class SimpleDetrender:
         return peak_per
     
 
+    def flatten(self,
+                gp_term,
+                nominal_gp_period,
+                minimum_gp_period,
+                transit_mask=None,
+                gap_tolerance=None,
+                jump_tolerance=None,
+                correct_ramp=True,
+                return_trend=False, 
+                progressbar=False
+               ):
+        """
+        Arguments
+            gp_term : str "RotationTerm" or "SHOTerm"
+            nominal_gp_period : float
+            minimum_gp_period : float
+            transit_mask : ndarray
+            gap_tolerance : int
+            jump_tolerance : float
+            correct_ramp : bool
+            return_trend : bool
+            progressbar : bool
+        """
+        # shorthand
+        lc = self.litecurve
 
-class GaussianProcessDetrender(SimpleDetrender):
-    def __init__(self):
-        pass
+        # sanitize inputs
+        if transit_mask is None:
+            transit_mask = np.zeros(len(lc.time), dtype=bool)
+        if gap_tolerance is None:
+            gap_tolerance = len(lc.time)
+        if jump_tolerance is None:
+            jump_tolerance = 1000.
 
-class AutocorrelationDetrender(SimpleDetrender):
-    def __init__(self):
-        pass
+        # identify breaks (gaps in time, jumps in flux)
+        # these will be used to identify contiguous "segments" of data
+        # the model will fit all n_seg segments simultaneously
+        # mean function (flux baseline + exponential trend) is independent for each segment
+        # gp parameters are shared between all segments
+        
+        break_locs = self._identify_breaks(gap_tolerance, jump_tolerance, mask=transit_mask)
+        
+        n_seg = len(break_locs) - 1
+        seg_id = np.zeros(len(lc.time), dtype=int)
+
+        for i in range(n_seg):
+            seg_id[break_locs[i]:break_locs[i+1]] = i
+
+        approx_mean = [np.mean(lc.flux[seg_id == i]) for i in range(n_seg)]
+
+        # convenience function
+        def _mean_fxn(time, seg_id, flux0, ramp_amp, log_tau):
+            nseg = len(np.unique(seg_id))
+            mean = T.zeros(len(time))
+
+            for i in range(nseg):
+                t_min = time[seg_id == i].min()
+                mean += (
+                    flux0[i]
+                    * (1 + ramp_amp[i] * T.exp(-(time - t_min) / T.exp(log_tau[i])))
+                    * (sig_id == i)
+                )
+
+            return mean
+        
+        # pymc model
+        with pm.Model() as model:
+            # set up the kernel
+            log_sigma = pm.Normal("log_sigma", mu=np.log(mad_std(lc.flux)), sd=5.0)
+            sigma = pm.Deterministic("sigma", T.exp(log_sigma))
+
+            log_P_off = pm.Normal("log_P", mu=np.log(nominal_gp_period - minimum_gp_period), sd=2.0)
+            P = pm.Deterministic("P", minimum_gp_period + T.exp(log_P_off))
+
+            log_Q0 = pm.Normal("log_Q0", mu=0.0, sd=5.0, testval=np.log(0.5))
+            Q0 = pm.Deterministic("Q0", T.exp(log_Q0))
+
+            if gp_term == 'RotationTerm':
+                log_dQ = pm.Normal("log_dQ", mu=0.0, sd=5.0, testval=np.log(1e-3))
+                mix = pm.Uniform("mix", lower=0, upper=1, testval=0.1)
+                kernel = GPterms.RotationTerm(sigma=sigma, period=P, Q0=Q0, dQ=T.exp(log_dQ), f=mix)
+
+            elif gp_term == 'SHOTerm':
+                kernel = GPterms.SHOTerm(sigma=sigma, w0=2 * pi / P, Q=0.5 + Q0)
+                
+            else:
+                raise ValueError(f"gp_term {gp_term} not recognized")
+            
+            # set up mean function and variance
+            flux0 = pm.Normal("flux0", mu=approx_mean, sd=np.ones(n_seg), shape=n_seg)
+            
+            if correct_ramp:
+                ramp_amp = pm.Normal("ramp_amp", mu=0, sd=np.std(lc.flux), shape=n_seg)
+                log_tau = pm.Normal("log_tau", mu=0, sd=5, shape=n_seg)
+            else:
+                ramp_amp = T.zeros(n_seg)
+                log_tau = T.zeros(n_seg)
+
+            # set up the gp
+            oot = ~transit_mask
+            mean_oot = pm.Deterministic("mean_oot", _mean_fxn(lc.time[oot], seg_id[oot], flux0, ramp_amp, log_tau))
+            mean_all = pm.Deterministic("mean_all", _mean_fxn(lc.time, seg_id, flux0, ramp_amp, log_tau))
+            log_var = pm.Normal("log_var", mu=np.var(lc.flux - median_filter(lc.flux, 13)), sd=5.0)
+
+            gp = GaussianProcess(kernel, 
+                                 t=lc.time[oot]
+                                 diag=T.exp(log_var)*T.ones(np.sum(oot))
+                                 mean=mean_oot
+                                )
+            gp.marginal("gp", observed=lc.flux[oot])
+
+        # optimize hyperparameters
+        with model:
+            map_soln = pmx.optimize(start=model.test_point, vars=[flux0], progress=progressbar)
+        
+
+
+
+
+
+
+
+        
+
+
+
+
+    
+
+class AutoCorrelationDetrender(SimpleDetrender):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- tests/test_detrend.py
+++ tests/test_detrend.py
@@ -3,10 +3,12 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import astropy
+from astropy.stats import mad_std
 import numpy as np
+from src.constants import *
 from src.schema.litecurve import LiteCurve
 from src.schema.planet import Planet
-from src.modules.detrend.detrend import SimpleDetrender
+from src.modules.detrend.detrend import GaussianProcessDetrender
 from src.utils.io import parse_koi_catalog, parse_holczer16_catalog
 import warnings
 
@@ -38,63 +40,81 @@ for i in range(NPL):
 data_dir = '/data/user/gjgilbert/data/MAST_downloads/'
 kic_id = catalog.kic_id[0]
 
-litecurve_raw = LiteCurve().load_kplr_pdcsap(data_dir, kic_id, 'long cadence')
-litecurve_list = litecurve_raw.split_quarters()
+litecurve_master_raw = LiteCurve().load_kplr_pdcsap(data_dir, kic_id, 'long cadence')
+litecurve_list_raw = litecurve_master_raw.split_quarters()
 
-for i, lc in enumerate(litecurve_list):
+for i, lc in enumerate(litecurve_list_raw):
     lc = lc.remove_flagged_cadences(bitmask='default')
 
-litecurve_clean = LiteCurve().from_list(litecurve_list)
+litecurve_master = LiteCurve().from_list(litecurve_list_raw)
 
-t_min = litecurve_clean.time.min()
-t_max = litecurve_clean.time.max()
+# check for negative timestamps
+t_min = litecurve_master.time.min()
+t_max = litecurve_master.time.max()
 
 if t_min < 0:
     raise ValueError("Lightcurve has negative timestamps...this will cause problems")
-
-litecurves = litecurve_clean.split_quarters()
-
-print(f"{len(litecurves)} litecurves loaded")
 
 # update planet ephemerides
 for n, p in enumerate(planets):
     if p.ephemeris is None:
         planets[n].ephemeris = p.predict_ephemeris(t_min, t_max)
 
-# detrend each quarter
+# split litecurves by quarter
+litecurves = litecurve_master.split_quarters()
+
 for j, litecurve in enumerate(litecurves):
     assert len(np.unique(litecurve.quarter)) == 1, "expected one quarter per litecurve"
     assert len(np.unique(litecurve.obsmode)) == 1, "expected one obsmode per litecurve"
 
+print(f"{len(litecurves)} litecurves loaded")
+
+
+# ################ #
+# DETRENDING BLOCK #
+# ################ #
+
+# initialize detrenders
 detrenders = []
 for j, litecurve in enumerate(litecurves):
     print(f"Quarter {litecurve.quarter[0]}")
+    detrenders.append(GaussianProcessDetrender(litecurve, planets))
 
-    detrender = SimpleDetrender(litecurve, planets)
-    mask = detrender.make_transit_mask(rel_size=3.0, abs_size=2/24)
-    mask = np.sum(mask, axis=0) > 0
-
+# clip outliers
+for j, detrender in enumerate(detrenders):    
+    mask = detrender.make_transit_mask(rel_size=3.0, abs_size=2/24, mask_type='condensed')
+    
     npts_initial = len(detrender.litecurve.time)
 
-    detrender.litecurve = detrender.clip_outliers(kernel_size=13,
-                                                  sigma_upper=5,
-                                                  sigma_lower=5,
-                                                  mask=mask
-                                                )
-    
-    detrender.litecurve = detrender.clip_outliers(kernel_size=13,
-                                                  sigma_upper=5,
-                                                  sigma_lower=1000,
-                                                  mask=None
-                                                 )
+    detrender.clip_outliers(kernel_size=13, sigma_upper=5, sigma_lower=5, mask=mask)
+    detrender.clip_outliers(kernel_size=13, sigma_upper=5, sigma_lower=1000, mask=None)
     
     npts_final = len(detrender.litecurve.time)
 
     print(f"  {npts_initial-npts_final} outliers rejected")
 
-    oscillation_period = detrender.estimate_oscillation_period(min_period=1.0)
+# estimate oscillation periods
+oscillation_periods = np.zeros(len(detrenders))
+for j, detrender in enumerate(detrenders):
+    obsmode = detrender.litecurve.obsmode[0]
 
-    print(f"  oscillation period = {oscillation_period:.1f} days")
+    if obsmode == 'short cadence':
+        min_period = np.max([5 * np.max(detrender.durs), 91 * kepler_scit])
+    elif obsmode == 'long cadence':
+        min_period = np.max([5 * np.max(detrender.durs), 13 * kepler_lcit])
+    else:
+        raise ValueError(f"unsuported obsmode: {obsmode}")
 
+    oscillation_periods[j] = detrender.estimate_oscillation_period(min_period=min_period)
+
+print(np.nanmedian(oscillation_periods))
+print(mad_std(oscillation_periods, ignore_nan=True))
+
+
+for j, detrender in enumerate(detrenders):
+    mask = detrender.make_transit_mask(rel_size=3.0, abs_size=2/24, mask_type='condensed')
+    break_locs = detrender._identify_breaks(gap_tolerance=13, jump_tolerance=5, mask=mask)
+
+    print(break_locs)
 
 print("passing")
