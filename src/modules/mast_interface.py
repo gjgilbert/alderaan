793ef9f47018fad649f7d5deeedecd060a361634
Greg Gilbert <gjgilbert@astro.ucla.edu>
2025-08-05 08:54:10 -0500

    pseudo-code instructions on mast_interface.py

--- src/modules/mast_interface.py
+++ src/modules/mast_interface.py
@@ -23,3 +23,17 @@ command = 'python /Users/research/projects/alderaan/bin/get_kepler_data.py {0} {
 
 print(command)
 #python /Users/research/projects/alderaan/bin/get_kepler_data.py 10748390 9570741 8826878 9141746 -c long --cmdtype wget
+
+# kplr010748390-2009131105131_llc.fits
+
+
+#### TUESDAY MORNING INSTRUCTIONS
+# user supplied K01234 (KOI_ID) and target file directory
+# use catalog to convert K01234 (KOI_ID) -> 12345678 (KIC_ID)
+# python get_kepler_data.py 9141746 -c long --cmdtype wget   | creates get_kepler_data.sh
+# get_kepler_data.sh | each line is a wget command for an expects kplr*.fits file
+# parse get_kepler_data.sh to determine what .fits files are expected
+# check for these files on disk
+# if they don't exist, download them to desired directory using wget commands
+# post: run a check that all files successfully downloaded and make another attempt if needed
+# log success/failure
