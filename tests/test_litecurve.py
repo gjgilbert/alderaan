329a055368ca883748c9637efb7492f08cd4e432
Sheila Sagear <ssagearastro@gmail.com>
2025-08-13 18:49:39 -0400

    added more K2 campaign tests

--- tests/test_litecurve.py
+++ tests/test_litecurve.py
@@ -20,18 +20,26 @@ base_path = Path(__file__).resolve().parents[1]
 data_dir = os.path.join(base_path, 'alderaan/examples/data/MAST_downloads/')
 kic_id = 8644288  # KOI-137 (Kepler-18)
 
-# # No quarters
-# litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence')
+# No quarters
+litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence')
 
-# # Integer quarter
-# litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence', quarters=1)
+# Integer quarter
+litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence', quarters=1)
 
-# # List quarters
-# litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence', quarters=[1,2,3])
+# List quarters
+litecurve = KeplerLiteCurve.load_kplr_pdcsap(data_dir, kic_id, 'long cadence', quarters=[1,2,3])
+
 
 epic_id = 211913977
 
-# K2: No quarters
+# K2: No campaign
 litecurve = K2LiteCurve.load_K2_everest(data_dir, epic_id, 'long cadence')
+
+# K2: Integer campaign
+litecurve = K2LiteCurve.load_K2_everest(data_dir, epic_id, 'long cadence', campaigns=5)
+
+# K2: List campaigns
+litecurve = K2LiteCurve.load_K2_everest(data_dir, epic_id, 'long cadence', campaigns=[5, 16, 18])
+
 
 print("\npassing")
